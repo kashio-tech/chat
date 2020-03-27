a1c57f385494f640c2e68b4416f832ac223b66d9
kashio-tech <gumgum.yk.29@gmail.com>
2020-03-27 19:22:30 +0900

    category_new complete

--- app/controllers/post_threads_controller.rb
+++ app/controllers/post_threads_controller.rb
@@ -5,6 +5,7 @@ class PostThreadsController < ApplicationController
 	end
 	def new
 		@post_thread = PostThread.new
+		binding.pry
 		@post_thread.post_thread_category_relations.build
 		@user = current_user
 	end
--- app/models/post_thread.rb
+++ app/models/post_thread.rb
@@ -1,6 +1,7 @@
 class PostThread < ApplicationRecord
 	belongs_to :user
 	has_many :post_comments
+	has_many :post_thread_category_relations
 	has_many :categories, through: :post_thread_category_relations
 	attachment :image
 end
